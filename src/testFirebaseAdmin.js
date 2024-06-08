88e411d522288df86a68ece23f7160068f209ff8
Miller Villamizar Cisneros <139370459+Millervillamizar@users.noreply.github.com>
2024-06-08 20:51:34 -0300

    06082024

--- src/testFirebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const admin = require('./config/firebaseAdmin');
-
-// Imprimir las variables de entorno para depuración
-console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
-console.log('FIREBASE_CLIENT_EMAIL:', process.env.FIREBASE_CLIENT_EMAIL);
-console.log('FIREBASE_PRIVATE_KEY:', process.env.FIREBASE_PRIVATE_KEY);
-
-console.log('Firebase Admin SDK initialized successfully');
