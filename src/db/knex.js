c4fe94424f717ad2b41b51b2da1baeee335cbdc9
Miller Villamizar Cisneros <139370459+Millervillamizar@users.noreply.github.com>
2024-06-11 20:47:09 -0300

    Update knex.js

--- src/db/knex.js
+++ src/db/knex.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const config = require('../db/knexfile');
 
-const db = knex(config.development);
+const environment = process.env.NODE_ENV || 'development';
+const db = knex(config[environment]);
 
 module.exports = db;
