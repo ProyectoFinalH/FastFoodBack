b4c134209f042e7af762fecab70966abfa1b2de1
Miller Villamizar Cisneros <139370459+Millervillamizar@users.noreply.github.com>
2024-06-11 17:44:51 -0300

    pathdir

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -1,23 +1,41 @@
-require("dotenv").config()
-const{DB_USER,DB_PASSWORD,DB_HOST,DB_NAME,DB_PORT}=process.env;
+require("dotenv").config();
+const path = require('path');
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 module.exports = {
-  
   development: {
     client: 'postgresql',
     connection: {
-      host: DB_HOST||'localhost',  
-      user: DB_USER||'postgres',
-      password: DB_PASSWORD||'Temporaltemporal1',
-      database: DB_NAME||'fast_food_app',
-      port: DB_PORT||'5432',
-      ssl: false // Asegúrate de que SSL esté deshabilitado si no lo necesitas
+      host: DB_HOST || 'localhost',
+      user: DB_USER || 'postgres',
+      password: DB_PASSWORD || 'Temporaltemporal1',
+      database: DB_NAME || 'fast_food_app',
+      port: DB_PORT || '5432',
+      ssl: false
     },
     migrations: {
-      directory: '../db/migrations',
+      directory: path.join(__dirname, 'migrations'),
     },
     seeds: {
-      directory: '../db/seeds',
+      directory: path.join(__dirname, 'seeds'),
     },
   },
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: 'monorail.proxy.rlwy.net',
+      port: 12776,
+      user: 'postgres',
+      password: 'XpuflCbtPuJIJFwMurEMgUESzeaspsHw',
+      database: 'railway',
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: path.join(__dirname, 'migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, 'seeds'),
+    },
+  }
 };
+
