e178507c77bfab220ba8a5853607eb8dab2c9177
fenixreds <114960950+fenixreds@users.noreply.github.com>
2024-06-13 00:45:06 -0500

    agrego cuenta superadmin data inicial

--- src/db/seeds/003_data_users_table.js
+++ src/db/seeds/003_data_users_table.js
@@ -2,14 +2,23 @@ exports.seed = async function(knex) {
     
     // Insertar nuevos registros
     await knex('users').insert([
-          {  
+        {  
             username: 'Usuario1',
             email: 'usuario1@gmail.com',
-            password: '1234',
+            password: '12345',
             telefono: '12345678',
             role_id:'1',
             active:true
         },
+        {
+            username: 'Administrador',
+            email: 'admin@gmail.com',
+            password: '12345',
+            telefono: '12345678',
+            role_id:'3',
+            active:true
+
+        }
                
     ]);
 };
