8ab85ab2ef0120b4e2b2c041f4287e079b7bb0ec
fenixreds <114960950+fenixreds@users.noreply.github.com>
2024-06-27 10:53:29 -0500

    delete and restore menuitems when delete categories

--- src/controllers/categoryController/deleteCategory.js
+++ src/controllers/categoryController/deleteCategory.js
@@ -3,6 +3,9 @@ const { findAndChange } = require("../../utils/findAndChange");
 const deleteCategory=async({id})=>{
     
     const {record,changed}=await findAndChange('categories',{id:id},{active:false});
+
+    const {recordItems,changedItems}=await findAndChange('menuitems',{category_id:id},{active:false});//para borrar los items de la categoria ocultada
+
    
     if(changed==true){
         return record;
--- src/controllers/categoryController/restoreCategory.js
+++ src/controllers/categoryController/restoreCategory.js
@@ -3,6 +3,8 @@ const { findAndChange } = require("../../utils/findAndChange");
 const restoreCategory=async({id})=>{
     
     const {record,changed}=await findAndChange('categories',{id:id},{active:true});
+
+    const {recordItems,changedItems}=await findAndChange('menuitems',{category_id:id},{active:true});//para restaurar los items de la categoria reactivada
    
     if(changed==true){
         return record;
